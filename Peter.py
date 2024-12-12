88cd1a3b07af029d0e79d893087f14093b4ab824
nophoswjhb024 <nophoswjhb024@student.wethinkcode.co.za>
2024-12-12 16:58:40 +0200

    Complete version

--- Peter.py
+++ Peter.py
@@ -3,31 +3,45 @@ import time
 
 t.bgcolor("black")
 t.color("white")
+t.penup()
+t.hideturtle()
 
-for i in range(3,0,-1):   
+t.teleport(0,0)
+t.write("🤡", align= "center",font=("Arial",100,"bold") )
+t.teleport(0,-200)
+t.write("Do you wanna play a game?", align= "center",font=("Arial",25,"bold") )
+time.sleep(2)
+t.clear()
+
+for i in range(3,0,-1): 
     t.teleport(0,-200)
     t.write(i,align="center",font=("Arial",250,"bold"))
     time.sleep(1)
     t.clear()
 
-bright_colors = [
+font_colors = [
     "Red",
-    "Green",
-    "Blue",
-    "Yellow",
-    "Magenta",
-    "Cyan",
     "Orange",
-    "Purple",
-    "Pink",
-    "Lime Green"
+    "Dark Red",
+    "Grey",
+    "Orange",
+    "Red",
+    "Orange",
+    "Grey",
+    "Red",
+    "Orange",
+    "Grey",
+    "White"
 ]
 
-for color in bright_colors:
-    t.teleport(0,-50)
+for color in font_colors:
     t.pencolor(color)
-    t.write("Happy Birthday!!!",align="center",font=("Arial",50,"bold"))
+    t.teleport(0,0)
+    t.write("👻🔪🎃",align="center",font=("Arial",100,"normal"))
+    t.teleport(0,-50)
+    t.write("Happy Birthday Peter!!!",align="center",font=("Arial",40,"bold"))
+    t.teleport(300, -300)
+    t.write("-From your favourites (Group 45/46)", align= "center",font=("Arial",15,"bold"))
     time.sleep(0.1)
 
-t.hideturtle()
 t.done()
